8233317a3dc709393f9884a3c32d4cb3462814d1
Kingster636 <42686284+Kingster636@users.noreply.github.com>
2021-02-14 14:53:16 -0600

    Update main.js to trigger new build

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const printMessage = (message) => {
   console.log(`Here is the message entered: '${message}'`);
 }
 
-printMessage('Devon King testing here Automated Jenkins Build');
+printMessage('Devon King testing here Automated Jenkins Build -- updated!');
